71a77a8ce27253f5cd77057963d3d1b6a1f22403
Hdaydich <abbassihend91@gmail.com>
2025-10-01 15:27:30 +0200

    Add files via upload

--- src/components/Chakel/Chakel.jsx
+++ src/components/Chakel/Chakel.jsx
@@ -1,26 +1,22 @@
-import { Card, CardImg, CardSubtitle, CardTitle } from "react-bootstrap";
+import { Card, CardImg, CardTitle } from "react-bootstrap";
 import s from "./style.module.css";
 import * as Icon from "react-bootstrap-icons";
-
-export function Chakel({ name, color, img, size = "19px" }) {
+export function Chakel({ name, color, img, size = "14px" }) {
   return (
     <Card className={`${s.CardContainer} border-0`}>
-      <CardImg variant="bottom" src={img} fluid></CardImg>
-
+      <CardImg variant="bottom" src={img} fluid />
       <CardTitle
         style={{
-          paddingBottom: "10px",
           color: color,
           textAlign: "center",
-          margin: "0px",
-          marginTop: "10px",
           fontSize: size,
+          margin: "0px",
+          marginTop: "6px",
         }}
       >
-        {name}
-        {"  "}
+        {name}{" "}
         <span className={s.icon}>
-          <Icon.CircleFill> </Icon.CircleFill>{" "}
+          <Icon.CircleFill />
         </span>
       </CardTitle>
     </Card>
